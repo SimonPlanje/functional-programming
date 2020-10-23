d6163a0ab6e9feaffcc394722628f63a5154929a
Simon <simon.planje2000@gmail.com>
2020-10-23 15:00:30 +0200

    functies alles naar capital, spaties weghalen en begin van het splitten van meerdere antwoorden toegevoegd

--- index.js
+++ index.js
@@ -1,23 +1,78 @@
 const deData = data;
 console.log(data);
 
+//DOORGEEFLUIK SCHONE ARRAYS>>
 //fix empty strings
 let dataList = fillEmptyStrings(deData);
+console.log("cleandata", dataList);
 
+let setToCaptial = bringAllToUpper(dataList);
+console.log("alles naar captial", setToCaptial);
+
+let whiteSpace = removeWhiteSpace(setToCaptial);
+console.log("whitespace", whiteSpace)
+
+
+let splitMultiples = splitMultipleAns(whiteSpace);
+console.log("splitted data", splitMultiples);
+
+
+//check politieke partij
+// let checkpolitics = removeStupidAwnsers(whiteSpace);
+
+
+//function for fixen empty strings
 function fillEmptyStrings(data){
     let cleanData = [];
 
     data.forEach(function(value){
-        if(value.politiekeVoorkeur.length < 3){
-            console.log(value.politiekeVoorkeur.length);
-            cleanData.push("GEEN VOORKEUR");
-        }else{
+        
+        if(value.politiekeVoorkeur.length < 2 || value.politiekeVoorkeur.length > 22 ) {
+            cleanData.push("GEENVOORKEUR");
+        }
+        else{
             cleanData.push(value.politiekeVoorkeur);
         }
     })
-    console.log("dit is de nieuwe array", cleanData)
+
+    return cleanData;
 }
 
+function bringAllToUpper(data){
+    let dataUpperCase = [];
+    data.map(function(value){
+        dataUpperCase.push(value.toUpperCase())
+        })
+        return dataUpperCase;    
+    }
+
+function removeWhiteSpace(data){
+    let dataNoSpaces = [];
+    data.map(function(value){
+        dataNoSpaces.push(value.replace(/\s/g, ''))
+//https://flaviocopes.com/how-to-replace-whitespace-javascript/#:~:text=The%20%5Cs%20meta%20character%20in,occurrence%20of%20the%20white%20space.
+    })
+    return dataNoSpaces;
+}
+
+
+function splitMultipleAns(data){
+    let splittedData = [];
+    data.map(function(value){
+        splittedData.push(value.split("," || "." || ";"));
+    })
+    return splittedData;
+}
+
+
+
+// function bringAllToUpper(data){
+//     data.forEach(function(value){
+//         if(value.politiekeVoorkeur)
+//     })
+// }
+
+// console.log("allesInCapital", toUpperKees);
 
 // let oogkleuren = deData.map(function(data){
 //     return data.oogKleur
